304331fcdb02e3852b9088038a2bd853f9ef7552
LeFengGe <lexuefeng@yeah.net>
2025-04-18 20:47:55 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -7,6 +7,7 @@
 #========================================================================================================================
 
 # Add a feed source
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # other
